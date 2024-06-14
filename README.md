f2c11ed431edb3c508148762e2a4e1d602d0e584
Even Torset <eventorset@gmail.com>
2024-06-14 03:23:49 +0200

    Reloader.

---
+++ README.md
@@ -0,0 +1,94 @@
+# FXR WebSocket Reloader
+This is a DLL mod for Elden Ring that allows FXR files to be reloaded while the game is still running.
+
+It hosts a WebSocket server that performs the reload when requested to. It can also be requested to change the resident SFX param fields of a weapon, which can be used to respawn SFX.
+
+## Installation
+You can download the mod from the [Releases page](https://github.com/EvenTorset/fxr-ws-reloader/releases/latest). To make the game load the DLL, you have two main options:
+
+### Mod Engine 2
+If you use [Mod Engine 2](https://github.com/soulsmods/ModEngine2/releases/latest), you can place the DLL and its config file anywhere, and then open the `config_eldenring.toml` file in a text editor and add the path to the DLL file to the `external_dlls` list, like this:
+```toml
+external_dlls = [
+  "C:\\your\\elden_ring\\dlls\\fxr-ws-reloader.dll"
+]
+```
+
+### Elden Mod Loader
+If you are using [Elden Mod Loader](https://www.nexusmods.com/eldenring/mods/117) to load DLL mods, simply place the DLL and its config file in your mods folder.
+
+## Configuration
+The JSON config file that comes with the DLL has some options that you can change to your needs:
+```json
+{
+  /*
+    Set this to true to enable the console, which will display extra
+    information about what the DLL is doing.
+  */
+  "log": false,
+
+  /*
+    This is the port number used for the WebSocket server. Feel free to change
+    it to whatever you'd like if you can't use the default for some reason.
+  */
+  "port": 24621
+}
+```
+
+## Usage
+The only way to control this reloader is through WebSocket requests.
+
+### fxr-reloader library
+The [fxr-reloader](https://www.npmjs.com/package/fxr-reloader) library was made to make this DLL mod easy to use from JavaScript so that scripts using [@cccode/fxr](https://www.npmjs.com/package/@cccode/fxr) to create or edit FXRs can put the FXRs directly into Elden Ring and have them respawn without manually interacting with the game at all. This allows these scripts to basically have a live preview of the effect that is being worked on.
+
+This library isn't the only thing that can control the reloader, however. If you want to create your own client to do so, the information you need is below.
+
+### Requests
+All requests to the WebSocket server should be JSON objects that include at least two properties:
+- `requestID`: A string used to identify the request. The server doesn't use this for anything. It simply includes it in the response to that request so that the client can know what request the response was for.
+- `type`: The type of the request, which tells the server what to do. It has two valid values:
+  - `0`: Reload FXR. This patches the definitions for the given FXR so that any new instances of it will use the new FXR. When this request type is used, the request needs one additional property:
+    - `data`: The binary data of the FXR encoded as a base64 string.
+  - `1`: Set resident SFX. This edits the resident SFX param fields for a given weapon based on the properties of the request. When this request type is used, the request needs three additional properties:
+    - `weapon`: The numerical ID of the weapon to edit. You can find a list of these here: https://github.com/MaxTheMiracle/Dark-Souls-3-Parts-Files/blob/master/Elden%20Ring
+    - `sfx`: The numerical SFX ID to change the `residentSfxId_1` param field to.
+    - `dmy`: The numerical dummy poly ID to change the `residentSfx_DmyId_1` param field to.
+
+#### Reload FXR example request payload
+```json
+{
+  "requestID": "example_request_1",
+  "type": 0,
+  "data": "<base64 goes here>"
+}
+```
+
+#### Set resident SFX example request payload
+```json
+{
+  "requestID": "example_request_2",
+  "type": 1,
+  "weapon": 24050000,
+  "sfx": 402030,
+  "dmy": 206
+}
+```
+
+### Responses
+The server will respond to all requests sent to it with a JSON object containing the request ID and a status message. The status message will always be "success" if the process succeeded. If it failed, the status message will describe why it failed in some way.
+
+#### Success example response
+```json
+{
+  "requestID": "example_request_2",
+  "status": "success"
+}
+```
+
+#### Error example response
+```json
+{
+  "requestID": "example_request_1",
+  "status": "Invalid FXR"
+}
+```
---
+++ fxr-ws-reloader.cpp
@@ -0,0 +1,151 @@
+#include "base.hpp"
+
+#include <Windows.h>
+
+#include <iostream>
+#include <fstream>
+
+#include <dantelion2/system.hpp>
+#include <detail/windows.inl>
+#include <coresystem/file/file.hpp>
+#include <coresystem/cs_param.hpp>
+#include <param/param.hpp>
+
+#include <websocketpp/config/asio_no_tls.hpp>
+#include <websocketpp/server.hpp>
+#include <websocketpp/base64/base64.hpp>
+
+#include <nlohmann/json.hpp>
+using json = nlohmann::json;
+
+extern "C" {
+  void patch_fxr(const char* process_name, const unsigned char* fxr_bytes, size_t fxr_size);
+}
+
+typedef websocketpp::server<websocketpp::config::asio> server;
+
+server ws_server;
+
+const LPCSTR ER_TITLE = "ELDEN RING\x99";
+const std::string LOG_PREFIX = "[fxr-ws-reloader] ";
+
+std::string getDLLDirPath() {
+  HMODULE hModule = GetModuleHandle("fxr-ws-reloader.dll");
+  if (hModule) {
+    char buffer[MAX_PATH];
+    DWORD length = GetModuleFileName(hModule, buffer, MAX_PATH);
+    if (length > 0) {
+      std::string fullPath(buffer);
+      size_t lastSlashPos = fullPath.find_last_of("\\/");
+      if (lastSlashPos != std::string::npos) {
+        return fullPath.substr(0, lastSlashPos);
+      }
+    }
+  }
+  return "";
+}
+
+std::string getEXEName() {
+  HMODULE hModule = GetModuleHandle(nullptr);
+  if (hModule) {
+    char buffer[MAX_PATH];
+    DWORD length = GetModuleFileName(hModule, buffer, MAX_PATH);
+    if (length > 0) {
+      std::string fullPath(buffer);
+      size_t lastSlashPos = fullPath.find_last_of("\\/");
+      if (lastSlashPos != std::string::npos) {
+        return fullPath.substr(lastSlashPos + 1);
+      }
+    }
+  }
+  return "unknown";
+}
+
+enum RequestType {
+  ReloadFXR = 0,
+  SetResidentSFX = 1,
+};
+
+void respond(websocketpp::connection_hdl hdl, json req, std::string status) {
+  json res {
+    { "requestID", req["requestID"] },
+    { "status", status },
+  };
+  ws_server.send(hdl, res.dump(), websocketpp::frame::opcode::text);
+}
+
+void on_message(websocketpp::connection_hdl hdl, server::message_ptr msg) {
+  json req = json::parse(msg->get_payload());
+  if (!req.contains("requestID")) {
+    json res {
+      { "status", "Missing request ID" },
+    };
+    ws_server.send(hdl, res.dump(), websocketpp::frame::opcode::text);
+    return;
+  }
+  if (!req.contains("type")) {
+    respond(hdl, req, "Missing request type");
+    return;
+  }
+  int req_type = req["type"];
+  switch (req_type) {
+    case RequestType::ReloadFXR: {
+      std::string binaryData = websocketpp::base64_decode(req["file"]);
+      const unsigned char* bdc = reinterpret_cast<const unsigned char*>(binaryData.c_str());
+      if (bdc[0] != 0x46 || bdc[1] != 0x58 || bdc[2] != 0x52 || bdc[3] != 0) {
+        std::cout << LOG_PREFIX << "Requested reload of invalid FXR" << '\n';
+        respond(hdl, req, "Invalid FXR");
+      }
+      uint32_t fxr_id = (bdc[15] << 24) | (bdc[14] << 16) | (bdc[13] << 8) | bdc[12];
+      patch_fxr(getEXEName().c_str(), bdc, binaryData.size());
+      std::cout << LOG_PREFIX << "Reloaded FXR: " << fxr_id << '\n';
+      respond(hdl, req, "success");
+      break;
+    }
+    case RequestType::SetResidentSFX: {
+      from::CS::SoloParamRepositoryImp::wait_for_params(-1);
+      int weaponID = req["weapon"];
+      bool foundWeapon = false;
+      for (auto [id, row] : from::param::EquipParamWeapon) {
+        if (id == weaponID) {
+          row.residentSfxId_1 = req["sfx"];
+          row.residentSfx_DmyId_1 = req["dmy"];
+          foundWeapon = true;
+          break;
+        }
+      }
+      if (foundWeapon) {
+        respond(hdl, req, "success");
+      } else {
+        respond(hdl, req, "Weapon not found");
+      }
+      break;
+    }
+    default:
+      std::cout << LOG_PREFIX << "Unrecognized request type: " << req["type"] << '\n';
+      respond(hdl, req, "Unrecognized request type");
+      break;
+  }
+}
+
+void reloader_main() {
+  std::ifstream f(getDLLDirPath() + "\\fxr-ws-reloader-config.json");
+  json config = json::parse(f, nullptr, true, true);
+
+  if (config["log"]) {
+    con_allocate(false);
+  }
+
+  from::DLSY::wait_for_system(-1);
+
+  ws_server.set_message_handler(&on_message);
+  ws_server.set_access_channels(websocketpp::log::alevel::none);
+  ws_server.set_error_channels(websocketpp::log::elevel::none);
+
+  ws_server.init_asio();
+  ws_server.listen(config["port"]);
+  ws_server.start_accept();
+
+  std::cout << LOG_PREFIX << "WebSocket server listening on port " << config["port"] << '\n';
+  ws_server.run();
+}
